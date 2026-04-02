5cd22ef13c9202af0cda766a6811e47e3f731146
Nicolas Pecoy <160270797+Nicolas-Pecoy@users.noreply.github.com>
2026-04-02 19:54:05 -0300

    fix

--- B1TuneUp/obj/Debug/net48/B1TuneUp.AssemblyInfo.cs
+++ B1TuneUp/obj/Debug/net48/B1TuneUp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("B1TuneUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266cfe9721e3eb1c8cc5728c1360aa6d2611ec2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1108cb37dede69e2df789a90f571f69bdbbdd7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("B1TuneUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("B1TuneUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
